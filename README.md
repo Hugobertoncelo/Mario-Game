a89cb2d280490588322831237635fcf4927cecd1
Hugobertoncelo <hugobertoncelo@gmail.com>
2025-01-23 09:20:03 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+<h1 align="center"> Mario Game </h1>
+
+<p align="center">
+  <a href="#-tecnologias">Technologies</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Project</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">License</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#readme-in-portuguese">README in Portuguese</a>
+</p> 
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+<br>
+<p align="center">
+  <img alt="Mario Game" src="./img/capa-mario.png" width="100%">
+</p>
+
+## 🚀 Technologies
+This project was developed with the following technologies:
+
+- HTML
+- CSS
+- JavaScript
+- Git and Github
+
+## 💻 Project
+
+Mario Game is a game where you can jump over obstacles. The game ends when you collide with an obstacle. The game is similar to Google's dinosaur game.
+
+## :memo: License
+This project is under the MIT license.
+
+by Hugo Bertoncelo
---
+++ scripts.js
@@ -0,0 +1,62 @@
+const mario = document.getElementById("mario");
+const pipe = document.getElementById("pipe");
+const gameover = document.getElementById("gameover");
+const pontuacao = document.getElementById("pontuacao");
+const record = document.getElementById("record");
+const atualizar = document.getElementById("atualizar");
+
+let pontuacaoAtual = 0
+let pontuacaoMaxima = localStorage.getItem("maxScore") || 0;
+
+const numberToString = parseInt(pontuacaoMaxima)
+record.textContent = "Record: " + numberToString.toFixed(0)
+
+function updateScore(pontos) {
+  if (pontos > pontuacaoMaxima) {
+    pontuacaoMaxima = pontos;
+    localStorage.setItem("maxScore", pontuacaoMaxima);
+  }
+}
+
+function jump() {
+  if (mario.classList != "jump") {
+    mario.classList.add("jump");
+
+    setTimeout(function () {
+      mario.classList.remove("jump");
+    }, 500);
+  }
+}
+
+let loop = setInterval(function () {
+
+  const pipePosition = parseInt(window.getComputedStyle(pipe).getPropertyValue("left"));
+  const marioPosition = parseInt(window.getComputedStyle(mario).getPropertyValue("bottom"));
+  pontuacaoAtual = pontuacaoAtual + 0.09;
+  const info = "Pontuação: " + pontuacaoAtual.toFixed(1);
+  pontuacao.textContent = info
+
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px`;
+    mario.src = './img/mario-gameover.webp';
+    mario.style.width = '120px';
+    mario.style.marginLeft = "40px";
+
+    gameover.style.marginLeft = "35%"
+    atualizar.style.marginLeft = "48%"
+
+    updateScore(pontuacaoAtual);
+
+    clearInterval(loop);
+  }
+
+}, 10);
+
+document.addEventListener("keydown", function (event) {
+  jump()
+});
